9205a8b2e300bbd2fdd2c4d39d280a43c2605a85
VasMax10 <60179065+VasMax10@users.noreply.github.com>
2020-04-30 11:11:37 +0300

    Stage 1 completed

---
+++ Models/Lab2FifaContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISTP_Lab2.Models
+{
+    public class Lab2FifaContext : DbContext
+    {
+        public virtual DbSet<Nation> Nations { get; set; }
+        public virtual DbSet<League> Leagues { get; set; }
+        public virtual DbSet<Club> Clubs { get; set; }
+        public virtual DbSet<CardType> CardTypes { get; set; }
+        public virtual DbSet<Card> Cards { get; set; }
+        public virtual DbSet<Player> Players { get; set; }
+        public Lab2FifaContext(DbContextOptions<Lab2FifaContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
